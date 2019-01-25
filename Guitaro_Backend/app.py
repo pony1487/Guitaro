77038585958e03ab03b5f366dbbe7ed256698a40
Ronan <connollyro@gmail.com>
2019-01-25 13:08:38 +0000

    Testing Debug on dev

--- Guitaro_Backend/app.py
+++ Guitaro_Backend/app.py
@@ -8,5 +8,11 @@ def hello_world():
     return 'Hello World!'
 
 
+@app.route('/testdebug')
+def test_debug():
+    print("debug works")
+    return 'testdebug'
+
+
 if __name__ == '__main__':
     app.run()
